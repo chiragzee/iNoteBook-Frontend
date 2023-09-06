7faeb2750365a99498f428dd8f5521349378cb8b
chiragzee <105245066+chiragzee@users.noreply.github.com>
2023-09-07 02:03:19 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -141,7 +141,7 @@ const Signup = (props) => {
               </Button>
               <Grid container>
                 <Grid item>
-                  <Link href="/login" variant="body2">
+                  <Link href="#" variant="body2">
                     Already have an account? Sign in
                   </Link>
                 </Grid>
